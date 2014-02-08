fa445c6e7b424521e71cabb00187b4ddbcbd4bff
Tran Anh Tuan <tk1cntt@gmail.com>
2014-02-08 21:07:35 +0700

    Support multi-platform and HD resolution

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -13,71 +13,43 @@
 
 USING_NS_CC;
 
-AppDelegate::AppDelegate()
-{
+AppDelegate::AppDelegate() {
 
 }
 
-AppDelegate::~AppDelegate()
-{
+AppDelegate::~AppDelegate() {
 }
 
-bool AppDelegate::initInstance()
-{
-    bool bRet = false;
-    do 
-    {
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
-
-        // Initialize OpenGLView instance, that release by CCDirector when application terminate.
-        // The HelloWorld is designed as HVGA.
-        CCEGLView * pMainWnd = new CCEGLView();
-        CC_BREAK_IF(! pMainWnd
-            || ! pMainWnd->Create(TEXT("cocos2d: Hello World"), 320, 480));
-
-#endif  // CC_PLATFORM_WIN32
-        
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-        // OpenGLView initialized in testsAppDelegate.mm on ios platform, nothing need to do here.
-#endif  // CC_PLATFORM_IOS
-
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
-		// android does not do anything
-#endif
-        
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WOPHONE)
-
-        // Initialize OpenGLView instance, that release by CCDirector when application terminate.
-        // The HelloWorld is designed as HVGA.
-        CCEGLView* pMainWnd = new CCEGLView(this);
-        CC_BREAK_IF(! pMainWnd || ! pMainWnd->Create(320,480));
-
-#ifndef _TRANZDA_VM_  
-        // on wophone emulator, we copy resources files to Work7/TG3/APP/ folder instead of zip file
-        cocos2d::CCFileUtils::setResource("HelloWorld.zip");
-#endif
-
-#endif  // CC_PLATFORM_WOPHONE
-
-        bRet = true;
-    } while (0);
-    return bRet;
-}
-
-bool AppDelegate::applicationDidFinishLaunching()
-{
+bool AppDelegate::applicationDidFinishLaunching() {
 	// initialize director
 	CCDirector *pDirector = CCDirector::sharedDirector();
-    pDirector->setOpenGLView(&CCEGLView::sharedOpenGLView());
 
-    // enable High Resource Mode(2x, such as iphone4) and maintains low resource on other devices.
-    // pDirector->enableRetinaDisplay(true);
+	pDirector->setOpenGLView(CCEGLView::sharedOpenGLView());
 
-	// sets landscape mode
-	// pDirector->setDeviceOrientation(kCCDeviceOrientationLandscapeLeft);
+	CCSize screenSize = CCEGLView::sharedOpenGLView()->getFrameSize();
+	CCSize designSize = CCSizeMake(480, 320);
+	std::vector<std::string> searchPaths;
+
+	if (screenSize.height > 320) {
+		searchPaths.push_back("hd");
+		searchPaths.push_back("sd");
+		pDirector->setContentScaleFactor(640.0f / designSize.height);
+	} else {
+		searchPaths.push_back("sd");
+		pDirector->setContentScaleFactor(320.0f / designSize.height);
+	}
+
+	CCFileUtils::sharedFileUtils()->setSearchPaths(searchPaths);
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT) || (CC_TARGET_PLATFORM == CC_PLATFORM_WP8)
+	CCEGLView::sharedOpenGLView()->setDesignResolutionSize(designSize.width, designSize.height, kResolutionShowAll);
+#else
+	CCEGLView::sharedOpenGLView()->setDesignResolutionSize(designSize.width,
+			designSize.height, kResolutionNoBorder);
+#endif
 
 	// turn on display FPS
-	pDirector->setDisplayFPS(true);
+	pDirector->setDisplayStats(true);
 
 	// set FPS. the default value is 1.0/60 if you don't call this
 	pDirector->setAnimationInterval(1.0 / 60);
@@ -92,19 +64,17 @@ bool AppDelegate::applicationDidFinishLaunching()
 }
 
 // This function will be called when the app is inactive. When comes a phone call,it's be invoked too
-void AppDelegate::applicationDidEnterBackground()
-{
-    CCDirector::sharedDirector()->pause();
+void AppDelegate::applicationDidEnterBackground() {
+	CCDirector::sharedDirector()->stopAnimation();
 
 	// if you use SimpleAudioEngine, it must be pause
 	// SimpleAudioEngine::sharedEngine()->pauseBackgroundMusic();
 }
 
 // this function will be called when the app is active again
-void AppDelegate::applicationWillEnterForeground()
-{
-    CCDirector::sharedDirector()->resume();
-	
+void AppDelegate::applicationWillEnterForeground() {
+	CCDirector::sharedDirector()->startAnimation();
+
 	// if you use SimpleAudioEngine, it must resume here
 	// SimpleAudioEngine::sharedEngine()->resumeBackgroundMusic();
 }
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -10,53 +10,59 @@
 
 // When you import this file, you import all the cocos2d classes
 #include "cocos2d.h"
-#include "Box2D.h"
+#include "Box2D/Box2D.h"
 
-class HelloWorld : public cocos2d::CCLayer {
+class HelloWorld: public cocos2d::CCLayer {
 public:
-    ~HelloWorld();
-    HelloWorld();
-    
-    // returns a Scene that contains the HelloWorld as the only child
-    static cocos2d::CCScene* scene();
-    
-    // adds a new sprite at a given coordinate
-    virtual void draw();
-    void tick(cocos2d::ccTime dt);
-    virtual void ccTouchesBegan(cocos2d::CCSet* touches, cocos2d::CCEvent* event);
-    virtual void ccTouchesMoved(cocos2d::CCSet* touches, cocos2d::CCEvent* event);
-    virtual void ccTouchesEnded(cocos2d::CCSet* touches, cocos2d::CCEvent* event);
-    void createBullets(int count);
-    bool attachBullet();
-    void resetBullet();
-    void resetGame();
-    
-    void createTargets();
-    void createTarget(const char *imageName, 
-                      cocos2d::CCPoint position, 
-                      float rotation, 
-                      bool isCircle, 
-                      bool isStatic, 
-                      bool isEnemy);
+	~HelloWorld();
+	HelloWorld();
+
+	virtual bool init();
+
+	// returns a Scene that contains the HelloWorld as the only child
+	static cocos2d::CCScene* scene();
+
+	CREATE_FUNC(HelloWorld)
+	;
+
+	// a selector callback
+	virtual void menuCloseCallback(cocos2d::CCObject* pSender);
+
+	// adds a new sprite at a given coordinate
+	virtual void draw();
+	void tick(float dt);
+	virtual void ccTouchesBegan(cocos2d::CCSet* touches,
+			cocos2d::CCEvent* event);
+	virtual void ccTouchesMoved(cocos2d::CCSet* touches,
+			cocos2d::CCEvent* event);
+	virtual void ccTouchesEnded(cocos2d::CCSet* touches,
+			cocos2d::CCEvent* event);
+	void createBullets(int count);
+	bool attachBullet();
+	void resetBullet();
+	void resetGame();
+
+	void createTargets();
+	void createTarget(const char *imageName, cocos2d::CCPoint position,
+			float rotation, bool isCircle, bool isStatic, bool isEnemy);
 
 private:
-    std::vector<b2Body *> m_bullets;
-    std::vector<b2Body *> m_targets;
-    std::vector<b2Body *> m_enemies;
+	std::vector<b2Body *> m_bullets;
+	std::vector<b2Body *> m_targets;
+	std::vector<b2Body *> m_enemies;
 
-    int m_currentBullet;
-    b2World* m_world;
-    b2Body* m_groundBody;
-    b2Fixture *m_armFixture;
-    b2Body *m_armBody;
-    b2RevoluteJoint *m_armJoint;
-    b2MouseJoint *m_mouseJoint;
-    
-    b2Body *m_bulletBody;
-    b2WeldJoint *m_bulletJoint;
-    
-    
-    bool m_releasingArm;
+	unsigned m_currentBullet;
+	b2World* m_world;
+	b2Body* m_groundBody;
+	b2Fixture *m_armFixture;
+	b2Body *m_armBody;
+	b2RevoluteJoint *m_armJoint;
+	b2MouseJoint *m_mouseJoint;
+
+	b2Body *m_bulletBody;
+	b2WeldJoint *m_bulletJoint;
+
+	bool m_releasingArm;
 };
 
 #endif // __HELLO_m_worldH__
